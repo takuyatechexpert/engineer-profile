6da4251b24b761ecedc4d667a8d536f80b72f6a4
takuyatechexpert <lp4546.sg.abc@gimail.com>
2020-01-14 20:46:14 +0900

    add devise
    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+devise_for :users
+root "profiles#index"
+
+  resources :profiles, only: :index
+  resources :users, only: [:edit, :update]
 end
