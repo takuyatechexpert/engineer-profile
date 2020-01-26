fefa075fb6faa2d7b0a6ed69fe689f1404c5973f
takuyatechexpert <lp4546.sg.abc@gimail.com>
2020-01-26 22:44:22 +0900

    fix seachForm
    fix Multiple keyword search

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,7 +5,25 @@ class ProfilesController < ApplicationController
   end
 
   def search
-    @users = User.search(params[:keyword]).where.not(id: current_user.id).order("created_at DESC")
+
+    redirect_to root_path if params[:keyword] == ""
+    keywords = params[:keyword].split(/[[:blank:]]+/)
+    @users = []
+    keywords.each do |search|
+    next if search == ""
+    @users += User.where('name LIKE(?) OR age LIKE(?) OR occupation LIKE(?) OR experience LIKE(?) OR programming_lang LIKE(?) OR pr LIKE(?)', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%").where.not(id: current_user.id).order("created_at DESC")
+    end
+    @users.uniq!
+    # @users = User.search(params[:keyword]).where.not(id: current_user.id).order("created_at DESC")
+
+    # return User.all unless search
+    # keywords = search.split(/[[:blank:]]+/)
+    # # @users = []
+    # keywords.each do |search|
+    # next search == ""
+    # User.where('name LIKE(?) OR age LIKE(?) OR occupation LIKE(?) OR experience LIKE(?) OR programming_lang LIKE(?) OR pr LIKE(?)', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+    # end
+
   end
 
 end
