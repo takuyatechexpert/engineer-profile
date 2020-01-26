123ce7ac95574798982678c96b9d4d73c2345850
takuyatechexpert <lp4546.sg.abc@gimail.com>
2020-01-26 14:23:08 +0900

    Refactoring
    scss Refactoring

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,7 +5,7 @@ class ProfilesController < ApplicationController
   end
 
   def search
-    @users = User.search(params[:keyword]).where.not(id: current_user.id)
+    @users = User.search(params[:keyword]).where.not(id: current_user.id).order("created_at DESC")
   end
 
 end
