de533f1c9864ff22049a11c25a514a8ef2e2b395
takuyatechexpert <lp4546.sg.abc@gimail.com>
2020-01-28 09:07:17 +0900

    refactoring
    fix view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -12,8 +12,8 @@ class ProfilesController < ApplicationController
     keywords.each do |search|
     next if search == ""
     @users += User.where('name LIKE(?) OR age LIKE(?) OR occupation LIKE(?) OR experience LIKE(?) OR programming_lang LIKE(?) OR pr LIKE(?)', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%").where.not(id: current_user.id).order("created_at DESC")
-    end
     @users.uniq!
+    end
     # @users = User.search(params[:keyword]).where.not(id: current_user.id).order("created_at DESC")
 
     # return User.all unless search
