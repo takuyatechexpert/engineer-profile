3d914fa44357d74a3a373fb7f7493c83e0de2657
takuyatechexpert <lp4546.sg.abc@gimail.com>
2020-01-28 09:45:35 +0900

    add  Refine search
    fix profiles_controller.rb

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,25 +5,19 @@ class ProfilesController < ApplicationController
   end
 
   def search
+    @keyword = params[:keyword]
 
-    redirect_to root_path if params[:keyword] == ""
-    keywords = params[:keyword].split(/[[:blank:]]+/)
-    @users = []
-    keywords.each do |search|
-    next if search == ""
-    @users += User.where('name LIKE(?) OR age LIKE(?) OR occupation LIKE(?) OR experience LIKE(?) OR programming_lang LIKE(?) OR pr LIKE(?)', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%").where.not(id: current_user.id).order("created_at DESC")
-    @users.uniq!
+    if @keyword.present?
+      @users = User.where.not(id: current_user.id).order("created_at DESC")
+      @keyword = params[:keyword].split(/[[:blank:]]+/)
+      
+      @keyword.each do |search|
+        next if search == ""
+        @users = @users.where('name LIKE(?) OR age LIKE(?) OR occupation LIKE(?) OR experience LIKE(?) OR programming_lang LIKE(?) OR pr LIKE(?)', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+        end
+    else
+      @users = User.where.not(id: current_user.id).order("created_at DESC")
     end
-    # @users = User.search(params[:keyword]).where.not(id: current_user.id).order("created_at DESC")
-
-    # return User.all unless search
-    # keywords = search.split(/[[:blank:]]+/)
-    # # @users = []
-    # keywords.each do |search|
-    # next search == ""
-    # User.where('name LIKE(?) OR age LIKE(?) OR occupation LIKE(?) OR experience LIKE(?) OR programming_lang LIKE(?) OR pr LIKE(?)', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
-    # end
-
   end
 
 end
