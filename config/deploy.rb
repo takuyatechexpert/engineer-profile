c4213bdbd8790c6547fa031aa19e6942666a776b
takuyatechexpert <lp4546.sg.abc@gimail.com>
2020-02-25 10:30:12 +0900

    fix deploy.rb
    rails 5.2に対応した記述に変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :repo_url,  'git@github.com:takuyatechexpert/engineer-profile.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 # secrets.yml用のシンボリックリンクを追加
-set :linked_files, %w{ config/secrets.yml }
+append :linked_files, 'config/database.yml', 'config/master.key'
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
