5380da87bd65c952a11313f57a3a99a7da1acb9e
takuyatechexpert <lp4546.sg.abc@gimail.com>
2020-02-25 18:14:15 +0900

    deploy.rb
    余分な記述を削除

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,19 +36,6 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
   
-  #   desc 'upload secrets.yml'
-  #   task :upload do
-  #     on roles(:app) do |host|
-  #       if test "[ ! -d #{shared_path}/config ]"
-  #         execute "mkdir -p #{shared_path}/config"
-  #       end
-  #       upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
-  #     end
-  #   end
-  #   before :starting, 'deploy:upload'
-  #   after :finishing, 'deploy:cleanup'
-  # end
-  
   desc 'upload master.key'
   task :upload do
     on roles(:app) do |host|
