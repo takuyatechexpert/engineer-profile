5dc43f29b872a0d01285713f5b152b1f977f3320
takuyatechexpert <lp4546.sg.abc@gimail.com>
2020-02-25 09:14:35 +0900

    fix deploy.rb
    deploy.rbを修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,9 @@ set :repo_url,  'git@github.com:takuyatechexpert/engineer-profile.git'
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
+# secrets.yml用のシンボリックリンクを追加
+set :linked_files, %w{ config/secrets.yml }
+
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
