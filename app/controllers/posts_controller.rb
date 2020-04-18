cfaa3fc96a3760d1f1720907f5ef05ad4e620ebb
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-04-19 00:11:35 +0900

    post system create
    新規投稿画面へ遷移して新規投稿できる様に機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,14 +5,19 @@ class PostsController < ApplicationController
   end
 
   def create
-
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to user_path(current_user)
+    else
+      render new_post_path
+    end
   end
 
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :comment, :image, :url).marge(user_id: current_user.id)
+    params.require(:post).permit(:title, :comment, :image, :url).merge(user_id: current_user.id)
   end
 
 end
