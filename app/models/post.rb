f3a15746f915a11cdcbc058a2407cd23d2c7e8c8
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-04-23 21:47:26 +0900

    add post-model validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,9 @@ class Post < ApplicationRecord
   belongs_to :user
 
   mount_uploader :image, ImageUploader
+
+  validates :title, presence: true
+  validates :comment, presence: true
+  validates :image, presence: true
+
 end
