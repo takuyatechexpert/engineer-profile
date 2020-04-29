c8e8f5a84d6467055df713903becac92f6328487
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-04-29 13:15:02 +0900

    add post faker

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :post do
-    
+    title {Faker::Book.title}
+    comment {Faker::Book.title}
+    image {Faker::Avatar.image(slug: "my-own-slug", size: "300x300", format: "jpg")}
+    url {Faker::Internet.url(host: 'example.com')}
   end
 end
