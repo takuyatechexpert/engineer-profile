99163ce421020535a199bd251c3977507f4abb62
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-04-29 14:03:48 +0900

    add post model test

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,7 +2,8 @@ FactoryBot.define do
   factory :post do
     title {Faker::Book.title}
     comment {Faker::Book.title}
-    image {Faker::Avatar.image(slug: "my-own-slug", size: "300x300", format: "jpg")}
+    image {File.open("#{Rails.root}/public/images/test_image.png")}
     url {Faker::Internet.url(host: 'example.com')}
+    user
   end
 end
